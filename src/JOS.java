9f2900dc167c611d5f0c8b6054c9134afe4dcc4f
flixname <felixpoetzsch@hotmail.de>
2022-06-14 18:19:34 +0200

    24
    JOS done
    JOS Tests done
    JOS Simulation main done

--- src/JOS.java
+++ src/JOS.java
@@ -1,21 +1,57 @@
+import domainLogic.ContentLogic;
 import domainLogic.MainLogic;
+import domainLogic.UploaderLogic;
+import mediaDB.Uploader;
+
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.concurrent.TimeUnit;
 
 public class JOS {
     public static void main(String[] args) {
-        MainLogic mainLogic = new MainLogic();
+        MainLogic mainLogic1 = new MainLogic();
+        System.out.println("Size of ContentLogic HashMap is now: " + mainLogic1.getUploaderLogic().getContentLogic().getAddressMediaContentHashMap().size());
 
-        System.out.println("Now saving to File.txt...");
+        ContentLogic contentLogic = new ContentLogic();
+
+        UploaderLogic uploaderLogic = new UploaderLogic();
+        uploaderLogic.setContentLogic(createSomeFiles());
+        mainLogic1.setUploaderLogic(uploaderLogic);
+
+
+        System.out.println("Now saving from MainLogic1 to File.txt...");
         //loading();
-        mainLogic.saveToFile();
+        mainLogic1.saveToFile();
 
-        System.out.println("Now trying to load data from File.txt...");
+        MainLogic mainLogic2 = new MainLogic();
+        System.out.println("Now trying to load data from File.txt into mainLogic2...");
         //loading();
 
-        mainLogic.loadFromFile("File.txt");
+        mainLogic2.loadFromFile("File.txt");
+
+        System.out.println("Size of ContentLogic HashMap in MainLogic2 should be 30 also, and is: " + mainLogic2.getUploaderLogic().getContentLogic().getAddressMediaContentHashMap().size());
     }
-/* For the lulz!
+
+    public static ContentLogic createSomeFiles() {
+
+        ContentLogic contentLogic = new ContentLogic();
+        for (int i = 0; i < 30; i++) {
+            try {
+                int randomNumber1 = ThreadLocalRandom.current().nextInt(1, 400);
+                int randomNumber2 = ThreadLocalRandom.current().nextInt(1, 200);
+
+                contentLogic.createContent("Audio", "defaultname", "News, Lifestyle", randomNumber1, randomNumber2);
+                System.out.println("created Media NR.: " + i);
+            } catch (IndexOutOfBoundsException e) {
+                throw new IndexOutOfBoundsException();
+            }
+        }
+        return contentLogic;
+    }
+
+/*
+// For the lulz! :)
     public static void loading(){
-        for (int i = 0; i < 40; i++) {
+        for (int i = 0; i < 30; i++) {
             try {
                 TimeUnit.MILLISECONDS.sleep(20);
             } catch (InterruptedException e) {
@@ -28,3 +64,4 @@ public class JOS {
 
  */
 }
+
